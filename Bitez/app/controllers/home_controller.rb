782964a7a4886aed2d1a1fe2c606ba61bf253282
Hunter Perschy <hperschy@uncc.edu>
2020-04-22 20:00:19 +0000

    Started implementing filter

--- Bitez/app/controllers/home_controller.rb
+++ Bitez/app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @restaurants = Style.where(nil)
+    @restaurants = @restaurants.filter_by_style(:TypeofFood).filter_by_price(:Price)
   end
 end
